5e4c137061c7fe41f38b27c1a03cbce90c490143
AliDalla <alidalla9500@gmail.com>
2025-01-10 18:22:22 +0300

    update settings

--- person_api/settings.py
+++ person_api/settings.py
@@ -143,8 +143,6 @@ MEDIA_ROOT = BASE_DIR / 'mediafiles'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',  # Allows all requests
