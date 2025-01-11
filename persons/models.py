ebebd5d512c821dd2a2cad7759e1196340c6d03a
AliDalla <alidalla9500@gmail.com>
2025-01-11 03:48:51 +0300

    update models

--- persons/models.py
+++ persons/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
+    address = models.CharField(max_length=100)
+    work = models.CharField(max_length=100)
+
 
     def __str__(self):
         return self.name
